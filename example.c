3dd4a93c486fb4bfac8b205d84f408aab1edaf1c
m1lkweed <m1lkweed@users.noreply.github.com>
2021-02-23 13:08:09 -0500

    Update example.c
    Used proper macro for stdout, rather than the fd

--- example.c
+++ example.c
@@ -9,11 +9,11 @@ int main(){
 	const char k8s_string[] = "Container detected\n";
 	const char  all_clear[] = "Nothing detected\n";
 	int clear = 0;
-	if(vmd_vmdetect())   {++clear; write(1,  vm_string, sizeof( vm_string));}
-	if(vmd_dbgpresent()) {++clear; write(1, dbg_string, sizeof(dbg_string));}
-	if(vmd_inchroot())   {++clear; write(1, chr_string, sizeof(chr_string));}
-	if(vmd_hardwaresus()){++clear; write(1, hws_string, sizeof(hws_string));}
-	if(vmd_incontainer()){++clear; write(1, k8s_string, sizeof(k8s_string));}
+	if(vmd_vmdetect())   {++clear; write(STDOUT_FILENO,  vm_string, sizeof( vm_string));}
+	if(vmd_dbgpresent()) {++clear; write(STDOUT_FILENO, dbg_string, sizeof(dbg_string));}
+	if(vmd_inchroot())   {++clear; write(STDOUT_FILENO, chr_string, sizeof(chr_string));}
+	if(vmd_hardwaresus()){++clear; write(STDOUT_FILENO, hws_string, sizeof(hws_string));}
+	if(vmd_incontainer()){++clear; write(STDOUT_FILENO, k8s_string, sizeof(k8s_string));}
 	
 	if(!clear){
 		write(1, all_clear, sizeof(all_clear));
